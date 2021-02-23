38c043aed2aa2e2bf03e7e435f2e7ec6e47df151
SeanWoo <kokarevvadim2002@gmail.com>
2021-02-23 18:32:07 +0400

    added get code

--- SpotifyBot/blueprints/api.py
+++ SpotifyBot/blueprints/api.py
@@ -1,15 +1,27 @@
 import uuid
+import requests as r
 from flask import session, request, jsonify
 from flask import Blueprint
 from extensions import db
+from SpotifyBot import BASE_DOMEN
 
 
 api_blueprint = Blueprint('api', __name__)
 
 @api_blueprint.route('/callback')
 def callback():
-    #if not session.get('uuid'):
-    #    session['uuid'] = str(uuid.uuid4())
     code = request.args.get("code")
+
+    access_token = request.args.get("access_token")
+    token_type = request.args.get("token_type")
+    scope = request.args.get("scope")
+    expires_in = request.args.get("expires_in")
+    refresh_token = request.args.get("refresh_token")
+
+    if code:
+        response = r.post(f"https://accounts.spotify.com/api/token?grant_type=authorization_code&code={code}&redirect_uri={BASE_DOMEN}/api/callback")
+
+    if access_token and refresh_token:
+        #TODO: сделать добавление в базу данных токенов
 
     return jsonify({'error_code': 'ok'})
