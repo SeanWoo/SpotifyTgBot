c652d50e8286329edd0ef57ceef00c7bf1b9e598
SeanWoo <52250045+SeanWoo@users.noreply.github.com>
2021-03-10 14:17:45 +0400

    fix test_session

--- Tests/test_session.py
+++ Tests/test_session.py
@@ -8,11 +8,11 @@ def test_session():
 
     initDb(db)
     tk = TokenRepository()
-    # tk.add_token(1,'185','135',3600)
-    # tk.add_token(2,'185','135',3600)
-    # tk.add_token(3,'185','135',3600)
-    # tk.add_token(4,'185','135',3600)
-    # tk.add_token(5,'185','135',3600)
+    tk.add_token(1,'185','135',3600)
+    tk.add_token(2,'185','135',3600)
+    tk.add_token(3,'185','135',3600)
+    tk.add_token(4,'185','135',3600)
+    tk.add_token(5,'185','135',3600)
     cache = session.Session(5)
 
     client1 = cache.take(1)
@@ -22,4 +22,4 @@ def test_session():
     client5 = cache.take(5)
     client6 = cache.take(6)
     client7 = cache.take(777)
-    assert client1.tgid == 1 and client2.tgid == 2 and client3.tgid == 3 and client4.tgid == 4 and client5.tgid == 5 and client6.tgid == 6 and client7 != 777
+    assert client1.tgid == 1 and client2.tgid == 2 and client3.tgid == 3 and client4.tgid == 4 and client5.tgid == 5 and client6 == None and client7 == None
