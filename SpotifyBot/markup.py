cdfe86b0b255637af716fca64b037dded85279f8
SeanWoo <52250045+SeanWoo@users.noreply.github.com>
2021-04-07 20:40:49 +0400

    fix bug with page manager

--- SpotifyBot/markup.py
+++ SpotifyBot/markup.py
@@ -63,9 +63,10 @@ def get_inline_control(user):
 def get_inline_playlist(user):
     markup = types.InlineKeyboardMarkup()
     
-    for playlist in user.pageManagerPlaylists[user.pageManagerPlaylists.page]:
-        button = types.InlineKeyboardButton(playlist.name, callback_data=f"selectPlaylist {playlist.id}")
-        markup.add(button)
+    if user.pageManagerPlaylists != None:
+        for playlist in user.pageManagerPlaylists[user.pageManagerPlaylists.page]:
+            button = types.InlineKeyboardButton(playlist.name, callback_data=f"selectPlaylist {playlist.id}")
+            markup.add(button)
 
     nav_prev_pl_button = types.InlineKeyboardButton('❮', callback_data="nav_prev_playlist")
     nav_page_pl_button = types.InlineKeyboardButton(f'{user.pageManagerPlaylists.page}/{user.pageManagerPlaylists.max_pages}', callback_data="nav_page_control")
@@ -78,12 +79,13 @@ def get_inline_playlist(user):
 def get_inline_tracks_of_playlist(user):
     markup = types.InlineKeyboardMarkup()
 
-    counter = 5*(user.pageManagerTracks.page - 1)
-    for track in user.pageManagerTracks[user.pageManagerTracks.page]:
-        counter += 1
-        button = types.InlineKeyboardButton(
-            text=f'{str(counter)}. {str(track.artists)} - {str(track.name)}', callback_data=f"selectTrackByPlaylistId {track.playlist_id} {counter}")
-        markup.add(button)
+    if user.pageManagerTracks != None:
+        counter = 5*(user.pageManagerTracks.page - 1)
+        for track in user.pageManagerTracks[user.pageManagerTracks.page]:
+            counter += 1
+            button = types.InlineKeyboardButton(
+                text=f'{str(counter)}. {str(track.artists)} - {str(track.name)}', callback_data=f"selectTrackByPlaylistId {track.playlist_id} {counter}")
+            markup.add(button)
 
     nav_prev_pl_button = types.InlineKeyboardButton('❮', callback_data="nav_prev_control")
     nav_page_pl_button = types.InlineKeyboardButton(f'{user.pageManagerTracks.page}/{user.pageManagerTracks.max_pages}', callback_data="nav_page_control")
@@ -96,12 +98,13 @@ def get_inline_tracks_of_playlist(user):
 def get_inline_search_tracks(user):
     markup = types.InlineKeyboardMarkup(row_width=1)
 
-    counter = 5*(user.pageManagerTracks.page - 1)
-    for track in user.pageManagerTracks[user.pageManagerTracks.page]:
-        counter += 1
-        button = types.InlineKeyboardButton(
-            text=f'{str(counter)}. {str(track.artists)} - {str(track.name)}', callback_data=f"selectTracks {counter}")
-        markup.add(button)
+    if user.pageManagerTracks != None:
+        counter = 5*(user.pageManagerTracks.page - 1)
+        for track in user.pageManagerTracks[user.pageManagerTracks.page]:
+            counter += 1
+            button = types.InlineKeyboardButton(
+                text=f'{str(counter)}. {str(track.artists)} - {str(track.name)}', callback_data=f"selectTracks {counter}")
+            markup.add(button)
 
     nav_prev_button = types.InlineKeyboardButton(
         '❮', callback_data="nav_prev_search")
